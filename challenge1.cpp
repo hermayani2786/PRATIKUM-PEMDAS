eb730b644ea614280d2f3b8bc9f354b7bee1cf45
hermayani2786 <herma.yani2786@gmail.com>
2026-03-03 20:27:01 +0700

    first commit

---
+++ challenge1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi variable
+float jarijari;
+float phi = 3.14159;
+
+//bagian fungsi
+void awal(){
+    cout << "masukkan jari-jari lingkaran:";
+    cin >> jarijari;
+}
+float luas (float jari_jari, float phi){
+    return phi *jarijari *jarijari;
+}
+void akhir (){
+    cout << "hasil luas untuk lingkaran dengan jari-jari" <<jarijari<<"adalah:" << luas(jarijari, phi);
+
+}
+int main(){
+    awal();
+    akhir();
+}
