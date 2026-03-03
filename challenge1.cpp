6c01f2d77db1667b5dc1c956c9da7da521a678f3
hermayani2786 <herma.yani2786@gmail.com>
2026-03-03 21:00:01 +0700

    menambahkan bagian fungsi

--- challenge1.cpp
+++ challenge1.cpp
@@ -4,3 +4,9 @@ using namespace std;
 //deklarasi variable
 float jarijari;
 float phi = 3.14159;
+
+//bagian fungsi
+void awal(){
+    cout << "masukkan jari-jari lingkaran:";
+    cin >> jarijari;
+}
